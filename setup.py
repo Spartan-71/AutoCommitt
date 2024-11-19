3e11cc161ddfedd739e20fc5977809210aea8ac4
Spartan-71 <anishdabhane71@gmail.com>
2024-11-20 00:32:34 +0530

    fix: update setup.py metadata

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name="autocommitt",
-    version="0.1.1",
-    author="Spartan (Anish Dabhane)",
-    author_email="<anishdabhane@gmail.com>",
+    version="0.1.3",
+    author="Anish Dabhane",
+    author_email="anishdabhane@gmail.com",
     description="A CLI tool for generating editable commit messages with local AI models",
     long_description=long_des,
     long_description_content_type='text/markdown',
@@ -23,13 +23,15 @@ setup(
         "local AI",
     ],
     classifiers=[
-        "Development Status :: Alpha version",
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
-        "Operating System :: Linux",
+        "Operating System :: Unix",
     ],
     entry_points={
         "console_scripts": ["autocommitt = autocommitt:hello", "ac = autocommitt:run"]
     },
-
+    url="https://github.com/Spartan-71/autocommitt",  # Update with your actual URL
+    license="Apache-2.0",  # Specify your license type
+    python_requires='>=3.8',  # Specify the minimum Python version required
 )
