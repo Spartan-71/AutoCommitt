7aa65709adf05fe9c8ca2313702ed1d6212e37c8
Spartan-71 <anishdabhane71@gmail.com>
2024-11-20 00:22:27 +0530

    feat: add README file to setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup, find_packages
 
+# Read the contents of your README file
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_des = f.read()
+
 setup(
     name="autocommitt",
     version="0.1.1",
     author="Spartan (Anish Dabhane)",
     author_email="<anishdabhane@gmail.com>",
     description="A CLI tool for generating editable commit messages with local AI models",
+    long_description=long_des,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
-    # install_requires=requirements,
     install_requires=["ollama"],
     keywords=[
         "autocommit",
